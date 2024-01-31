dde72ad699018fef424413780349c0047200f3db
vamsimadhav <vamsimadhav2000@gmail.com>
2024-01-31 07:30:14 +0530

    Android Branch Sdk Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import { useEffect } from 'react';
+import { BranchDeepLinks } from 'capacitor-branch-deep-links';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -23,13 +25,35 @@ import './theme/variables.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Menu from './pages/Menu';
+import { Browser } from '@capacitor/browser';
 
-setupIonicReact({
-  // mode: 'ios',
-  // animated: false,
-});
+setupIonicReact();
 
-const App: React.FC = () => (
+const App = () => {
+  useEffect(() => {
+    branchListener();
+  }, [])
+
+  const openWebview = async (canonical_url:string) => {
+    await Browser.open({ url: canonical_url });
+  };
+
+  const branchListener = () => {
+    BranchDeepLinks.addListener('init', (event) => {
+      console.log(`[branch.io] Success to initialize: ${JSON.stringify(event.referringParams)}`);
+      if (event.referringParams.$canonical_url) {
+        console.log(`[branch.io] $canonical_url : ${event.referringParams.$canonical_url}`);
+        //openWebview(event.referringParams.$canonical_url);
+        Browser.open({ url: event.referringParams.$canonical_url });
+      }
+    });
+    
+    BranchDeepLinks.addListener('initError', (error) => {
+      console.log(`[branch.io] Fails to initialize: ${error}`);
+    });
+  }
+
+ return (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
@@ -41,6 +65,7 @@ const App: React.FC = () => (
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
-);
+  );
+ }
 
 export default App;
